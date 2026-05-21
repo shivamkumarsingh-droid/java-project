4ae20e6949310b57c4c3d9b6360425bd4b2cc549
Shivam Kumar Singh <shivam.kumar.singh@rungta.org>
2026-05-21 12:13:03 +0530

    Add files via upload

---
+++ shivam.java
@@ -0,0 +1,54 @@
+class Player {
+    private String playerName;
+    private int score;
+
+    public Player(String playerName, int score) {
+        this.playerName = playerName;
+        this.score = score;
+    }
+
+    public String getName() {
+        return playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
+
+public class GameSystem {
+    public static void main(String[] args) {
+
+        // Creating player objects
+        Player[] players = {
+            new Player("Rahul", 95),
+            new Player("Ankit", 88),
+            new Player("Priya", 82),
+            new Player("Neha", 76)
+        };
+
+        for (int i = 0; i < players.length - 1; i++) {
+
+            for (int j = 0; j < players.length - i - 1; j++) {
+
+                if (players[j].getScore() < players[j + 1].getScore()) {
+
+                    Player temp = players[j];
+                    players[j] = players[j + 1];
+                    players[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Players Ranked by Score:");
+
+        for (int i = 0; i < players.length; i++) {
+            System.out.println(
+                (i + 1) + ". " +
+                players[i].getName() +
+                " - Score: " +
+                players[i].getScore()
+            );
+        }
+    }
+}
